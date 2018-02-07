2d97d5cf7c9345005b81a10e6c74503390a2d991
TyrionJYQ <1003518719@qq.com>
2018-02-08 00:43:37 +0800

    deal-css-in-webpack folder

---
+++ deal-css-in-webpack/demo 01/app.js
@@ -0,0 +1,2 @@
+import base from './src/css/base.css'
+import common from './src/css/common.css'
---
+++ deal-css-in-webpack/demo 01/webpack.config.js
@@ -0,0 +1,29 @@
+var path = require('path')
+module.exports = {
+  entry: {
+    app: './app.js'
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: './dist/',
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].chunk.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use:[
+          {
+            // loader: 'style-loader'
+            loader: 'style-loader/url'
+          },
+          {
+            // loader: 'css-loader'
+            loader: 'file-loader'
+          }
+        ]
+      }
+    ]
+  }
+}
