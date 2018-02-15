fee4870aed29af5000b9c45b4b98310b8930ebab
TyrionJYQ <1003518719@qq.com>
2018-02-15 15:20:18 +0800

    demo

---
+++ tree-shaking/jquey-tree-shaking/app.js
@@ -0,0 +1,9 @@
+import base from './src/css/base.css'
+import common from './src/css/common.css'
+
+import {a} from './src/common/util'
+console.log(a())
+
+import {ajax} from 'jquery'
+$.ajax(url,data);
+
